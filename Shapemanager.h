ebec5fa138b1744ac28b6d4fd9368d389590ab18
bluesteel202400528 <ama194650.2007@gmail.com>
2025-08-06 16:10:03 +0300

    Update Shapemanager.h

--- Shapemanager.h
+++ Shapemanager.h
@@ -2,28 +2,37 @@
 using namespace std;
 #include <string>
 #include <cmath>
+#include "CMUgraphicsLib/CMUgraphics.h"
 class ShapeManager :public window,public display {
 public:
+    
     string selshape(int px, int py, int rx1, int ry1, int rx2, int ry2,
         int tx1, int tx2, int tx3, int ty1, int ty2, int ty3,
         int cx, int cy, int radius) {
+        
+        int dx = px - cx;
+        int dy = py - cy;
+        string shapeuse="NONE";
 
         if (px >= rx1 && px <= rx2 && py >= ry1 && py <= ry2) {
-            return "RECTANGLE";
+            shapeuse = "RECTANGLE";
         }
         else if (px >= min(tx1, min(tx2, tx3)) && px <= max(tx1, max(tx2, tx3)) &&
             py >= min(ty1, min(ty2, ty3)) && py <= max(ty1, max(ty2, ty3))) {
-            return "TRIANGLE";
+            shapeuse =  "TRIANGLE";
         }
-        else {
-            int dx = px - cx;
-            int dy = py - cy;
-            if (dx * dx + dy * dy <= radius * radius) {
-                return "CIRCLE";
+      
+         else if (dx * dx + dy * dy <= radius * radius) {
+                shapeuse = "CIRCLE";
             }
+        else{
+            shapeuse="NONE";   
         }
-        return "NONE";
-    }
+        return shapeuse;
+        }
+
+
+    
 
     string copy(int px, int py, int rx1, int ry1, int rx2, int ry2,
         int tx1, int tx2, int tx3, int ty1, int ty2, int ty3,
@@ -61,12 +70,11 @@ public:
         else {
             copiedShape = "NONE";
         }
-
          return copiedShape;
     }
 
 
-    void paste(int offsetX = 30, int offsetY = 30) {
+    void paste(int offsetX = 30, int offsetY = 30, &window w) {
         if (copiedShape == "CIRCLE") {
             w.DrawCircle(ccx + offsetX, ccy + offsetY, cr, FILLED);
         }
@@ -79,6 +87,8 @@ public:
                 ctx2 + offsetX, cty2 + offsetY,
                 ctx3 + offsetX, cty3 + offsetY, FILLED);
         }
+        shapestate="NONE";
+        copiedShape="NONE";
     }
 
 private:
